b0a9680b59b6e77de9aa017f7d286909cccab593
Captain Comics <captain.comics@stangoodman.info>
2020-02-19 16:15:52 +0000

    Adds rudimentary Debian/RHEL detection

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -46,8 +46,19 @@ else
     platform=$(uname);
     # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
+      if [ $(grep -c debian /etc/os-release) -gt 0 ]; then 
+        # Debian/Ubuntu based, use apt
         sudo apt-get install zsh
         install_zsh
+      elif [ $(grep -c rhel /etc/os-release) -gt 0 ]; then 
+        # Redhat/Centos based, use yum 
+        sudo yum install zsh
+        install_zsh
+      else 
+        echo "Unable to detect type of Linux. Doesn't look like Debian or Redhat."
+        echo "Please install zsh manually, and rerun this setup."
+        exit
+      fi
     # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
         echo "Please install zsh, then re-run this script!"
