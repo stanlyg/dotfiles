fcb610c62392d5f2e93a99d8adf2cc9dd8d422b4
Captain on ubutest1 <captain@stanlyg.com>
2025-03-18 16:47:03 +0000

    Separate ZSH installation from oh-my-zsh installation

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,25 +4,17 @@
 # This script installs zsh, if it's not already installed
 ############################
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone https://github.com/robbyrussell/oh-my-zsh.git
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
+  # Test to see if zshell is installed.  If it is:
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    echo "Zsh seems to already be installed."
+  else
     # If zsh isn't installed, get the platform of the current machine
     platform=$(uname);
     # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
       if [ $(grep -c debian /etc/os-release) -gt 0 ]; then 
         # Debian/Ubuntu based, use apt
-        sudo apt-get install zsh
-        install_zsh
+        sudo apt-get install zsh zsh_common zsh_doc
       elif [ $(grep -c rhel /etc/os-release) -gt 0 ]; then 
         # Redhat/Centos based, use yum 
         sudo yum install zsh
@@ -32,12 +24,13 @@ else
         echo "Please install zsh manually, and rerun this setup."
         exit
       fi
-    # If the platform is OS X, tell the user to install zsh :)
+      # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
+      echo "You must be running a really old version of MacOS -- ZSH is normally installed by default."
+      echo "Please install zsh, then re-run this script!"
+      exit
     fi
-fi
+  fi
 }
 
 install_zsh
